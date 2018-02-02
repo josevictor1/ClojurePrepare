049e2487f91c66624d06ab1524af4e89bf6ff224
José Victor <josevictorpatos@gmail.com>
2018-02-02 17:39:27 -0200

    done

--- learningclojure/src/learningclojure/core.clj
+++ learningclojure/src/learningclojure/core.clj
@@ -1,12 +1,42 @@
 (ns learningclojure.core
   (:gen-class))
 
-(print-str "Hello, World!")
-(print-str "Hello, World!" " " "from Clojure")
+;;(print-str "Hello, World!")
+;;(print-str "Hello, World!" " " "from Clojure")
 ;; (quot x y) will give you the whole number part of x divided by y.
 ;; (rem x y) will give you the remainder of x divided by y.
 
+
+(defn reverse-recursively [coll]
+  (loop [r (rest coll)
+         acc (conj () (first coll))]
+    (if (= (count r) 0)
+      acc
+      (do
+        (print acc)
+        (recur (rest r) (conj acc (first r)))))))
+
+(defn reverse-recursively [coll]
+  (loop [[r & more :as all] (seq coll)
+         acc '()]
+    (if all
+      (recur more (cons r acc))      
+      acc)))
+
+
+;;my solution
+(defn callmyreverse [coll] (myreverse coll () (count coll)))
+
+(defn myreverse [mylist x n]
+  (if (= (count x) n)
+    x (recur (rest mylist) (conj x (first mylist)) n))) 
+  
+;; Returns a lazy sequence of x, (f x), (f (f x)) etc. f must be free of side-effects
+(take 5 (iterate inc 5))
+(#(take % (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))) 5)
+  
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& args])
+  
